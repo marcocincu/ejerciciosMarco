df974774e234699b883734ab6a9f4164baa48bf9
Marco Cincunegui <ceibal@96fx5900038h80123693.ingresolaptops.ceibal.edu.uy>
2021-05-27 23:41:02 -0300

    primer commit

---
+++ 1-ejercicio/js/ejercicio1.js
@@ -0,0 +1,9 @@
+
+const frase = "\n You should not wast time in the Internet<br></br>";
+
+let contador = 1;
+while(contador<1001){
+    document.write(contador);
+    document.write(frase);
+    contador += 1;
+}
