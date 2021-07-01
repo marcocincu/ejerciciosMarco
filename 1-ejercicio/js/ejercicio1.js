953b6de347a74e4fa506d32e32827c58c0778efb
marcocincu <79168631+marcocincu@users.noreply.github.com>
2021-07-01 15:20:43 -0300

    Arreglando y abreviando algunos codigos

--- 1-ejercicio/js/ejercicio1.js
+++ 1-ejercicio/js/ejercicio1.js
@@ -1,9 +1,8 @@
 
-const frase = "\n You should not wast time in the Internet<br></br>";
+const frase = " " + " You should not wast time in the Internet</br>";
 
 let contador = 1;
 while(contador<1001){
-    document.write(contador);
-    document.write(frase);
+    document.write(contador, frase);
     contador += 1;
 }
